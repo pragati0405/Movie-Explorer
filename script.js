92e1201c92c4781f54d7c0787057c3cdf83c108a
pragati0405 <pragativerma154@email.com>
2026-07-16 00:16:11 +0530

    Added search results and started trending movies feature

--- script.js
+++ script.js
@@ -2,16 +2,26 @@
 
  let searchbox=document.querySelector("#searchInput");
  let searchButton=document.querySelector("#searchButton");
-let searchsection=document.querySelector(".searchSection");
-let trendingSection=document.querySelector(".trendingSection");   
+let searchsection=document.querySelector("#searchSection");
+let trendingSection=document.querySelector("#trendingSection"); 
+let searchResultsList=document.querySelector("#searchResultsList");
+let trendingMoviesList=document.querySelector("#trendingMoviesList");
+
+
  searchButton.addEventListener("click", function() {
     if(searchbox.value.trim() !== "") {
-        let movie = searchbox.value.trim();
-        fetch(`https://www.omdbapi.com/?apikey=4ebbc37&s==${movie}`)
+        let query = searchbox.value.trim();
+        fetch(`https://www.omdbapi.com/?apikey=4ebbc37&s=${query}`)
         .then(response => response.json())
-        .then((data)=>{
-            createMovieCards(data.Search);
-             })
+        .then((data)=>{  
+             
+             searchsection.style.display = "block";
+            trendingSection.style.display = "none";
+            searchResultsList.innerHTML = "";
+           data.Search.forEach(function(movie) {
+                createMovieCard(movie, searchResultsList);
+            });
+                    })
         .catch((error)=>{
             console.error("Error fetching movie data:", error);
         });
@@ -22,22 +32,48 @@ let trendingSection=document.querySelector(".trendingSection");
  });
  
  
- function createMovieCards(movies) {
-     document.querySelector(".movie-grid").innerHTML = "";
-     searchsection.style.display = "block";
-     trendingSection.style.display = "none";
-    let moviecard=document.createElement("div");
+ function createMovieCard(movie,containerId) {
+      let moviecard=document.createElement("div");
     moviecard.classList.add("movie-card");
     moviecard.innerHTML=`
+    <img src="${movie.Poster}" alt="${movie.Title} Poster" class="movie-poster">
+    <h3 class="movie-title">${movie.Title}</h3>
+    <p class="movie-year">Year: ${movie.Year}</p>
+    <p class="movie-type">Type: ${movie.Type}</p>
+    `;
 
-   document.querySelector(".movie-grid").appendChild(moviecard);
+   containerId.appendChild(moviecard);
  };
  
- data.Search.forEach(function(movie){
-                console.log(movie.Title);
-                console.log(movie.Year);
-                console.log(movie.Type);
-                console.log(movie.Poster);
-                console.log(" -------------------");
+ 
 
-            });
+//  trending movies section
+const trendingQueries = [
+  "Batman",
+  "Avengers",
+  "Spider Man",
+  "Harry Potter",
+  "Inception",
+  "Interstellar",
+  "Joker",
+  "Titanic",
+  "Fast"
+];
+
+searchsection.style.display = "none";
+ trendingSection.style.display = "block";
+let randomIndex = Math.floor(Math.random() * trendingQueries.length);
+let randomQuery = trendingQueries[randomIndex];
+fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${randomQuery}`)
+ .then(response => response.json())
+ .then((data)=>{  
+            document.querySelector("#trendingMoviesList").innerHTML = "";
+            data.Search.forEach(function(movie) {
+              createMovieCard(movie,trendingMoviesList);
+        });
+ })
+ .catch((error)=>{
+            console.error("Error fetching movie data:", error);
+        });
+ 
+  
